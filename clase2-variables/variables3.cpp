34d170a4954a725d173a4c4856382c0a0bbe0973
kjaimes <kjaimes@unal.edu.co>
2019-09-11 08:58:48 -0500

    Val1-Val2-info

---
+++ clase2-variables/variables3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(void)
+{
+  int val1=0;
+  int val2=0;
+  std::cout << "Ingrese dos valores" << std::endl;
+  std::cout << "valor 1: " ; std::cin >> val1 ;
+  std::cout << "valor 2: " ; std::cin >> val2 ;
+  std::cout << "la informacion de sus valores es :" ;
+  return 0;
+
+}
